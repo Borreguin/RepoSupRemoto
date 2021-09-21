109131a1a60511b2b831c63d1dbd12bdf39d8386
sistemaremoto <61017667+sistemaremoto@users.noreply.github.com>
2021-09-21 09:30:32 -0500

    Eliminacion Carpetas

--- flask_app/dto/mongo_engine_handler/SRFinalReport/sRFinalReportPermanente.py
+++
@@ -1,5 +0,0 @@
-from flask_app.dto.mongo_engine_handler.SRFinalReport.sRFinalReportBase import SRFinalReportBase
-
-
-class SRFinalReportPermanente(SRFinalReportBase):
-    meta = {"collection": "REPORT|FinalReports"}
--- flask_app/dto/mongo_engine_handler/SRNodeReport/sRNodeReportPermanente.py
+++
@@ -1,5 +0,0 @@
-from flask_app.dto.mongo_engine_handler.SRNodeReport.sRNodeReportBase import SRNodeDetailsBase
-
-
-class SRNodeDetailsPermanente(SRNodeDetailsBase):
-    meta = {"collection": "REPORT|Nodos"}
